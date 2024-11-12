6b3c0dc38021f54c5aec282a073ef4cb241f201b
Gabrielle-w05 <gabriellewestbrook05@gmail.com>
2024-11-11 21:38:49 -0600

    added my  pizza api to spa

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function render(state = store.home) {
       ${main(state)}
       ${footer()}
     `;
-    router.updatePageLinks();
+  router.updatePageLinks();
 }
 
 router.hooks({
@@ -29,29 +29,29 @@ router.hooks({
       // Add a case for each view that needs data from an API
       case "home":
         axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis&units=imperial`).then(response => {
-          console.log("Weather Response Data", response.data);
-          store.home.weather = {
-            city: response.data.name,
-            temp: response.data.main.temp,
-            feelsLike: response.data.main.feels_like,
-            description: response.data.weather[0].main
-          };
-          done();
-        }).catch(error => {
-          console.log("It puked", error);
-          done();
-        });
-      break;
+          .get(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis&units=imperial`).then(response => {
+            console.log("Weather Response Data", response.data);
+            store.home.weather = {
+              city: response.data.name,
+              temp: response.data.main.temp,
+              feelsLike: response.data.main.feels_like,
+              description: response.data.weather[0].main
+            };
+            done();
+          }).catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
       case "pizza":
         // New Axios get request utilizing already made environment variable
         axios
-        .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
           .then(response => {
             // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
             console.log("response", response);
             store.pizza.pizzas = response.data;
-            
+
 
             done();
           })
@@ -72,6 +72,55 @@ router.hooks({
     render(store[view]);
   },
   after: match => {
+    const view = match?.data?.view ? camelCase(match.data.view) : "home";
+
+    if (view === "order") {
+      // Add an event handler for the submit button on the form
+      document.querySelector("form").addEventListener("submit", event => {
+        event.preventDefault();
+
+        // Get the form element
+        const inputList = event.target.elements;
+        console.log("Input Element List", inputList);
+
+        // Create an empty array to hold the toppings
+        const toppings = [];
+
+        // Iterate over the toppings array
+
+        for (let input of inputList.toppings) {
+          // If the value of the checked attribute is true then add the value to the toppings array
+          if (input.checked) {
+            toppings.push(input.value);
+          }
+        }
+
+        // Create a request body object to send to the API
+        const requestData = {
+          customer: inputList.customer.value,
+          crust: inputList.crust.value,
+          cheese: inputList.cheese.value,
+          sauce: inputList.sauce.value,
+          toppings: toppings
+        };
+        // Log the request body to the console
+        console.log("request Body", requestData);
+
+        axios
+          // Make a POST request to the API to create a new pizza
+          .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
+          .then(response => {
+            //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+            store.pizza.pizzas.push(response.data);
+            router.navigate("/pizza");
+          })
+          // If there is an error log it to the console
+          .catch(error => {
+            console.log("It puked", error);
+          });
+      });
+    }
+
     router.updatePageLinks();
 
     // add menu toggle to bars icon in nav bar
@@ -95,7 +144,7 @@ router.on({
       render(store.viewNotFound);
 
       console.log(`View ${view} not defined`);
-    } 
+    }
   },
 }).resolve();
 
